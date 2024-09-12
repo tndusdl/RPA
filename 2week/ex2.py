7c04be1f8b1596a2e7d26229e90fad6f3d0d309c
[tndusdl] <ksy04120@naver.com>
2024-09-12 12:30:53 +0900

    2주차 실습파일

---
+++ 2week/ex2.py
@@ -0,0 +1,10 @@
+a = 123
+b = 15.556
+
+print("a:[0] b:[1]" .format(a,b))
+print(f"a:[a] b:[b]")
+
+print(f"a:[a:0.5d] b:{b:2f}")
+print("a:%05d b:%.2f" % (a,b))
+
+
---
+++ 2week/ex3.py
@@ -0,0 +1,21 @@
+print("2개의 숫자를 입력하세요.")
+num1 = int(input())
+num2 = int(input())
+num3 =num1 + num2
+print(num3)
+
+num4 = num1 // 3
+print("num1 // 3 ", num4)
+
+a = 5
+if a == 5:
+    print('Right!')
+    print('a is 5')
+
+if a == 3:
+     print('Right!')
+     print('a is 3')
+     
+if a != 3:
+     print('Right!')
+     print('a is not 3')
