9005533e1867cbdb5e59a209067d9c214ab142ba
수연 <162692823+tndusdl@users.noreply.github.com>
2024-11-21 12:27:12 +0900

    Update ex4.py

--- 11week/ex4.py
+++ 11week/ex4.py
@@ -4,7 +4,7 @@ name = input("이름을 입력하세요: ")
 stdtnum = input("학번을 입력하세요: ")
 major = input("전공을 입력하세요: ")
 
-qr_data = [name, stdtnum, major]
+qr_data = f"{name} {stdtnum} {major}" 
 qr_img = qrcode.make(qr_data)
 
 save_path = 'my_info_data.png'
