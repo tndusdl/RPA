dd2fb5b4082aa0c1100d9704409b4d7ddfe1e234
수연 <162692823+tndusdl@users.noreply.github.com>
2024-11-28 14:08:39 +0900

    Update ex1.py

--- 12week/ex1.py
+++ 12week/ex1.py
@@ -1,32 +1,22 @@
 import pandas as pd
-
-w = pd.read_csv("ch5-1.csv")
-print(w.head(), end='\n\n')
-
 from sklearn.model_selection import train_test_split
 
+w = pd.read_csv('ch5-1.csv')
 x_data = w.iloc[:, 2:5].values
 y_data = w.iloc[:, 1].values
 
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
-print(len(pd.DataFrame(x_train)), len(pd.DataFrame(x_test)), end='\n\n')
-print(len(pd.DataFrame(y_train)), len(pd.DataFrame(y_test)), end='\n\n')
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2) 
 
 from sklearn.neural_network import MLPRegressor
 model_mlp = MLPRegressor().fit(x_train, y_train)
-
-print(model_mlp.get_params(), end='\n\n')
-
 y_pred_mlp = model_mlp.predict(x_test)
-print(y_train, end='\n\n')
-print(y_pred_mlp, end='\n\n')
 
-df_x_test = pd.DataFrame(x_test, columns=['egg_weight', 'movement', 'food'])
+df_x_test = pd.DataFrame(x_test, columns=['egg_weight', 'movement','food'])
 df_y_pred = pd.DataFrame(y_pred_mlp, columns=['predict'])
-df_y_test = pd.DataFrame(y_test, columns=['real'])
+df_y_test = pd.DataFrame(y_test, columns=['real']) 
 df = pd.concat([df_x_test, df_y_test, df_y_pred], axis=1)
 print(df, end='\n\n')
 
-from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score
+from sklearn.metrics import  r2_score
 R2 = r2_score(y_test, y_pred_mlp)
 print("R2 = ", R2, end='\n\n')
