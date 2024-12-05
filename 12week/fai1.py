e945a2615b53731224bc172caaa807bfe54c3185
수연 <162692823+tndusdl@users.noreply.github.com>
2024-12-05 11:30:41 +0900

    Update fai1.py

--- 12week/fai1.py
+++ 12week/fai1.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI, Form, File, UploadFile 
+from fastapi import FastAPI, Form, File, UploadFile, HTTPException
 import shutil
 from pathlib import Path
-from fastapi.responses import FileResponse
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse, StreamingResponse
 import qrcode
 from io import BytesIO
 from fastapi.staticfiles import StaticFiles
 import qrcode.constants
+import sqlite3
 
 app = FastAPI()
 
@@ -20,8 +20,7 @@ async def get_file(filename: str):
         raise HTTPException(status_code=404, detail="File not found")
 
 @app.post("/qrcode/")
-async def generate_qr(data:str = Form(...)):
-    
+async def generate_qr(data: str = Form(...)):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -31,15 +30,15 @@ async def generate_qr(data:str = Form(...)):
     qr.add_data(data)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color='black', back_color ='white')
-    
+    img = qr.make_image(fill_color='black', back_color='white')
+
     # 바이너리 데이터로 변환
     img_byte_arr = BytesIO()
     img.save(img_byte_arr, format='PNG')
     img_byte_arr = img_byte_arr.getvalue()
-    
-    return StreamingResponse(BytesIO(img_byte_arr), media_type="image")
-    
+
+    return StreamingResponse(BytesIO(img_byte_arr), media_type="image/png")
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     save_path = Path("static/uploads") / file.filename
@@ -50,7 +49,32 @@ async def create_upload_file(file: UploadFile = File(...)):
 
     return {"filename": file.filename, "location": str(save_path)}
 
+def loginDB(userId: str, userPassword: str):
+    conn = sqlite3.connect('education.db')
+    cursor = conn.cursor()
 
+    query = 'SELECT * FROM user WHERE userId = ? AND userPassword = ?'
+    cursor.execute(query, (userId, userPassword))
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        print("Login successful!")
+        print("User Info:", result)
+        return True
+    else:
+        print("Login failed. Invalid username or password.")
+        return False
+
+@app.post("/login")
+def login_form(userid: str = Form(...), userpassword: str = Form(...)):
+    result = loginDB(userid, userpassword)
+    if result:
+        return {"msg": f"{userid}님 반갑습니다."}
+    else:
+        return {"msg": "로그인에 실패했습니다."}
+
+# 정적 파일 서빙
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == '__main__':
